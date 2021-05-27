2502949e38ac10691a3ec9607728873d7aefe796
Walaamohammad <>
2021-05-27 18:12:27 +0000

    add java

---
+++ script.js
@@ -0,0 +1,17 @@
+var name ="walaa";
+Console.log(name);
+if(userName=="walaa"){
+console.log("Hello walaa")
+ }else{
+console.log("Hello "+ Name)
+ }
+ var age=20
+
+if(age == 25 ){
+console.log("what do you work ?");
+ }else if(age<=10){
+ console.log("go back to school !!");
+}else{
+ console.log("welcome to our website");
+ }
+ 
