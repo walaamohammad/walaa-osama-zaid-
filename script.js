31db28162a8629063ee92dbb584f144d55ff69ce
Walaamohammad <>
2021-05-27 19:05:58 +0000

    add

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
-var name ="walaa";
-Console.log(name);
-if(userName=="walaa"){
-console.log("Hello walaa")
- }else{
-console.log("Hello "+ Name)
- }
- var age=20
 
-if(age == 25 ){
-console.log("what do you work ?");
- }else if(age<=10){
- console.log("go back to school !!");
-}else{
- console.log("welcome to our website");
- }
  
+var userName = "walaa";
+var hXh = "swimming";
+var hop ="hiking";
+var userName = prompt("Whats your name ?");
+alert("welcome to our websit " + userName )
+
+
+var favhoppy = prompt("What is your fav hoppy ?");
+
+if(favhoppy === hXh ){
+
+  document.write("<p> Welcome to the club </p>")
+}else if(favhoppy === "hiking"){
+  document.write('<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSUd-zT5MIO4DM0QvP-CwXrQSw0p0ZmKWHdA&usqp=CAU"/>')
+}else{
+  alert('please next time answer either swimming or hiking ')
+}
+
+
+
+
+
+
