62145d0b4913c82fb596d97ea455b6b2c2e478b3
Walaamohammad <>
2021-05-28 13:57:12 +0000

    add js file

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 
- 
 var userName = "walaa";
-var hXh = "swimming";
-var hop ="hiking";
+
+
+//var hoppy = "hiking"
+var hop = "driving"
 var userName = prompt("Whats your name ?");
-alert("welcome to our websit " + userName )
+alert ("welcome to our websit " + userName )
 
 
-var favhoppy = prompt("What is your fav hoppy ?");
+var favhoppy = prompt("What is your fav hoppy  ?");
 
-if(favhoppy === hXh ){
 
-  document.write("<p> Welcome to the club </p>")
-}else if(favhoppy === "hiking"){
-  document.write('<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSUd-zT5MIO4DM0QvP-CwXrQSw0p0ZmKWHdA&usqp=CAU"/>')
-}else{
-  alert('please next time answer either swimming or hiking ')
-}
+
+if(favhoppy === "hiking") {
+
+ alert("your hopp is intersting")
+  
+}else if(favhoppy === "driving") {
+  alert("your hoop is driving")
+  
+}else { document.write  ( '<p> welcome to my website </p>')}
 
 
 
